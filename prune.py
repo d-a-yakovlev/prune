dd63ed8d29d37d0df5b1672f6b1c8a3f22e70292
d-a-yakovlev <raidegdp@gmail.com>
2022-07-30 17:58:18 +0300

    d-a-y/refactoring

--- prune.py
+++ prune.py
@@ -1,35 +1,15 @@
 import sys
-import os
+
+from src_prune.base_prune import Prune
 
 '''
--d параметр для католога dir (e.g -d contest1)
--t параметр для задачи task (e.g -t A)
+1) параметр для католога dir (e.g contest1)
+2) параметр для задачи task (e.g A)
 
-Запуск будет следующий python prune.py -d contest1 -t A
+Запуск будет следующий python prune.py contest1 A
 '''
 
-def ans_check (data_arr, test_count):
-    fh = open(case_data_name, "w")
-    fh.close()
-
-    fh = open(case_data_name, "a")
-    for data in data_arr:
-        fh.write(data)
-    
-    fh.close()
-
-    brute_ans = os.popen(f"python {brute_solution} < {case_data_name}").readlines()
-    opt_ans   = os.popen(f"python {opt_solution} < {case_data_name}").readlines()
-
-    if brute_ans == opt_ans:
-        print (f"Test {test_count} : OK")
-    else:
-        print (f"Test {test_count} : Not OK \n  {data_arr}")
-
-
-
-
-_, f_d, dir_name, f_t, task_name = sys.argv
+_, dir_name, task_name = sys.argv
 
 task_data_name = dir_name + "/" + task_name + "_data.txt"
 brute_solution = dir_name + "/" + task_name + "_brute.py"
@@ -37,6 +17,8 @@ opt_solution   = dir_name + "/" + task_name + "_opt.py"
 
 data_sep = "/* end */"
 case_data_name = dir_name + "/" + task_name + "_case_data.txt"
+
+tester = Prune(case_data_name, brute_solution, opt_solution)
 
 # Читать из task_data_name до разделителя, потом передать эти данные в решения
 data_file = open(task_data_name, "r")
@@ -48,7 +30,7 @@ for line in data_file:
     if line != data_sep:
         input_data.append(line)
     else:
-        ans_check(input_data, test_count)
+        tester.classic_check(input_data, test_count)
         test_count += 1
         input_data.clear()
 
---
+++ src_prune/base_prune.py
@@ -0,0 +1,44 @@
+import os
+
+'''
+Класс как пространство имён
+статичных методов для валидации
+'''
+class Prune:
+    
+    '''
+    Конструктор
+    Принимает:
+    - case_data_name - имя файла для буфера
+    - brute_solution - имя файла не оптимального решения
+    - opt_solution - имя файла оптимального решения
+    '''
+    def __init__(self, case_data_name, brute_solution, opt_solution):
+        self.case_data_name = case_data_name
+        self.brute_solution = brute_solution
+        self.opt_solution = opt_solution        
+
+    
+    '''
+    Классический подход
+    Принимает:
+    - data_arr - данные для одного тестового случая
+    - test_count - номер теста
+    '''
+    def classic_check (self, data_arr, test_count):
+        fh = open(self.case_data_name, "w")
+        fh.close()
+
+        fh = open(self.case_data_name, "a")
+        for data in data_arr:
+            fh.write(data)
+        
+        fh.close()
+
+        brute_ans = os.popen(f"python {self.brute_solution} < {self.case_data_name}").readlines()
+        opt_ans   = os.popen(f"python {self.opt_solution} < {self.case_data_name}").readlines()
+
+        if brute_ans == opt_ans:
+            print (f"Test {test_count} : OK")
+        else:
+            print (f"Test {test_count} : Not OK \n  {data_arr}")
