0a2e559d483ad5a4db1279ac9adb9e93ee12d8c2
entoshik <entoshik@gmail.com>
2022-08-13 14:15:11 +0300

    v 1.2

--- prune.py
+++ prune.py
@@ -4,26 +4,22 @@ from src_prune.base_prune import Prune
 '''
 1) параметр для католога dir (e.g contest1)
 2) параметр для задачи task (e.g A)
+3) 
 
 Запуск будет следующий python prune.py contest1 A
 '''
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--directory', help='', required=True)
-parser.add_argument('-t', '--task', help='', required=True)
-parser.add_argument('-s', '--solution', help='', action='store_true')
+parser.add_argument('-d', '--directory', help='The name of the directory where the files are stored', required=True)
+parser.add_argument('-t', '--task', help='task name', required=True)
+parser.add_argument('-s', '--solution', help='Set the -s flag if there '
+                                             'is no native solution, but there is an output file.', action='store_true')
 args = parser.parse_args()
 
-task_data_name = directory + "/" + task + "_data.txt"
-brute_solution = directory + "/" + task + "_brute.py"
-opt_solution = directory + "/" + task + "_opt.py"
+tester = Prune(directory, task)
 
 data_sep = "/* end */"
-case_data_name = dir_name + "/" + task_name + "/" + "_case_data.txt"
-
-tester = Prune(case_data_name, brute_solution, opt_solution)
-
 # Читать из task_data_name до разделителя, потом передать эти данные в решения
-with open(task_data_name, "r") as da_file:
+with open(task_data_name, "r") as data_file:
     test_count = 1
     input_data = []
     for line in data_file:
@@ -32,8 +28,8 @@ with open(task_data_name, "r") as da_file:
             input_data.append(line)
         else:
             if solution:
-                tester.classic_check(input_data, test_count)
-            else:
                 tester.check_with_solution(input_data, test_count)
+            else:
+                tester.classic_check(input_data, test_count)
             test_count += 1
             input_data.clear()
--- src_prune/base_prune.py
+++ src_prune/base_prune.py
@@ -8,36 +8,36 @@ import os
 
 class Prune:
 
+
     '''
     Конструктор
     Принимает:
-    - case_data_name - имя файла для буфера
-    - brute_solution - имя файла не оптимального решения
-    - opt_solution - имя файла оптимального решения
+    - directory - имя директории, в которой хранятся файлы
+    - task - ммя задачи
     '''
-    def __init__(self, case_data_name, brute_solution, opt_solution):
-        self.case_data_name = case_data_name
-        self.brute_solution = brute_solution
-        self.opt_solution = opt_solution
+
+    def __init__(self, directory, task):
+        self.directory = directory
+        self.task = task
+        self.task_data_name = directory + "/" + task + "_data.txt"
+        self.brute_solution = directory + "/" + task + "_brute.py"
+        self.opt_solution = directory + "/" + task + "_opt.py"
     '''
     Классический подход
     Принимает:
     - data_arr - данные для одного тестового случая
     - test_count - номер теста
     '''
-    def classic_check (self, data_arr, test_count):
+    def classic_check(self, data_arr, test_count):
 
-        with open(self.case_data_name, 'w') as fh:
-            for data in data_arr:
-                fh.write(data)
-
-        brute_ans = os.popen(f"python3 {self.brute_solution} < {self.case_data_name}").readlines()
-        opt_ans = os.popen(f"python3 {self.opt_solution} < {self.case_data_name}").readlines()
+        data_str = '\n'.join(data_arr)
+        brute_ans = os.popen(f"echo {data_str} | python3 {self.brute_solution}").readlines()
+        opt_ans = os.popen(f"echo {data_str} | python3 {self.opt_solution}").readlines()
 
         if brute_ans == opt_ans:
             print (f"Test {test_count} : OK")
         else:
-            f = open(wrong.txt, 'a')
+            f = open(f'{directory}/wrong.txt', 'a')
             print (f"Test {test_count} : Not OK \n  "
                    f"{data_arr}\n "
                    f"correct answer: {brute_ans}\n "
@@ -45,11 +45,10 @@ class Prune:
             f.close()
 
     def check_with_solution(self,  data_arr, test_count):
-        with open(self.case_data_name, 'w') as fh:
-            for data in data_arr:
-                fh.write(data)
 
-        opt_ans = os.popen(f"python3 {self.opt_solution} < {self.case_data_name}").readlines()
+        data_str = '\n'.join(data_arr)
+        opt_ans = os.popen(f"echo {data_str} | python3 {self.opt_solution}").readlines()
+        brute_ans =
 
         if brute_ans == opt_ans:
             print(f"Test {test_count} : OK")
